134c8f70449736ca6e1b8834b8a9a7e2d50aad4a
Jake O'Rielly <jake.orielly@gmail.com>
2019-09-07 15:23:14 -0400

    Enemies and collision in dungeon

--- dungeon.vue
+++ dungeon.vue
@@ -1,3 +1,11 @@
+let playerToken = {x:1, y:3, img: 'images/player.png'};
+let playerTurn = true;
+let playerSteps = 2;
+let enemies = [
+  {x:3,y:3,img:'images/enemy.png'},
+  {x:5,y:5,img:'images/enemy.png'}
+];
+
 Vue.component('dungeon', {
   props: {
     app: Object,
@@ -6,29 +14,52 @@ Vue.component('dungeon', {
     return {
         rows: 10,
         cols: 10,
-        playerX: 1,
-        playerY: 3,
         floor: 'images/floor.png',
-        player: 'images/player.png'
     }
   },
   methods: {
     // Vue reactivity is great for lots of things but it's not as fast as JS
-    renderPlayer: function() {
-      document.getElementById(this.playerY + ':' + this.playerX).innerHTML = this.getTile() + '<img src="' + this.player + '">';
+    renderToken: function(token) {
+      document.getElementById(token.y + ':' + token.x).innerHTML = this.getTile() + '<img src="' + token.img + '">';
     },
-    erasePlayer: function() {
-      document.getElementById(this.playerY + ':' + this.playerX).innerHTML = this.getTile();
+    eraseToken: function(token) {
+      document.getElementById(token.y + ':' + token.x).innerHTML = this.getTile();
     },
     movePlayer: function(x,y) {
-      let newX = this.playerX + x;
-      let newY = this.playerY + y;
-      if (newX > 0 && newX <= this.rows && newY > 0 && newY <= this.cols) {
-        this.erasePlayer();
-        this.playerX += x;
-        this.playerY += y;
-        this.renderPlayer();
+      let newX = playerToken.x + x;
+      let newY = playerToken.y + y;
+      if (playerTurn && newX > 0 && newX <= this.rows && newY > 0 && newY <= this.cols && !this.collision(newX,newY,playerToken)) {
+        playerSteps--;
+        if (playerSteps == 0) {
+          playerTurn = false;
+          this.enemyTurn();
+        }
+        this.eraseToken(playerToken);
+        playerToken.x += x;
+        playerToken.y += y;
+        this.renderToken(playerToken);
+        document.getElementById("player-steps").innerHTML = playerSteps;
       }
+    },
+    enemyTurn: function() {
+      this.renderToken(enemies[0]);
+      playerSteps = 2;
+      playerTurn = true;
+    },
+    collision: function(x,y,token) {
+      let curr;
+      if (token != playerToken) {
+        curr = playerToken;
+        if (curr.x == x && curr.y == y)
+          return true;
+      }
+      for (let i = 0; i < enemies.length; i++)
+        if (enemies[i] != token) {
+          curr = enemies[i];
+          if (curr.x == x && curr.y == y)
+            return true;
+        }
+      return false;
     },
     keyPress: function() {
       if (event.key == 'w')
@@ -55,8 +86,10 @@ Vue.component('dungeon', {
     for (var i = 1; i <= this.cols; i++)
       for (var j = 1; j <= this.rows; j++)
         document.getElementById(i + ':' + j).innerHTML = this.getTile();
-    this.renderPlayer();
-    document.addEventListener("keyup", this.keyPress);
+    this.renderToken(playerToken);
+    for (let i = 0; i < enemies.length; i++)
+      this.renderToken(enemies[i]);
+    document.addEventListener("keydown", this.keyPress);
   },
   template: `<div>
     <table cellspacing=0>
@@ -65,5 +98,6 @@ Vue.component('dungeon', {
             </td>
         </tr>
     </table>
+    <div id="player-steps">2</div>
   </div>`
 })
